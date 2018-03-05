5a1cc34fc8b557e502ceb32886eff2a9246535ad
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-03-05 14:49:27 -0300

    adding node_modules and stuff

--- binary-search-tree/demo/src/index.ts
+++ binary-search-tree/demo/src/index.ts
@@ -1,15 +1,5 @@
 import { BinarySearchTree } from '../../BinarySearchTree';
 
-let tree = new BinarySearchTree<number>();
-let treeSize = tree.size;
-let documentBody = document.body;
-
-let sizeParagraph = document.createElement("P");
-let sizeText = document.createTextNode(`Current size: ${treeSize}`);
-
-sizeParagraph.appendChild(sizeText);
-documentBody.appendChild(sizeParagraph)
-
 function refreshScreen() {
 
 }
@@ -18,3 +8,17 @@ function insert(value: number) {
   tree.insert(value);
   refreshScreen();
 }
+
+function main() {
+  let sizeParagraph = document.createElement("P");
+  let sizeText = document.createTextNode(`Current size: ${treeSize}`);
+
+  sizeParagraph.appendChild(sizeText);
+  documentBody.appendChild(sizeParagraph)
+}
+
+let tree = new BinarySearchTree<number>();
+let treeSize = tree.size;
+let documentBody = document.body;
+
+main();
