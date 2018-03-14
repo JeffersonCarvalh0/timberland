e235486bb09744d419c3893d5ca736a9b39e4f7c
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-03-14 17:54:17 -0300

    Started to implement the tree

---
+++ n-tree/test/NTree.spec.ts
@@ -0,0 +1,21 @@
+import { NTree } from "../NTree";
+import { assert } from "chai";
+import "mocha";
+
+describe('Testing NTree class', function() {
+  let tree = new NTree();
+
+  describe('constructor', function() {
+    it('Should return a valid NTree object', function() {
+      assert.instanceOf(tree, NTree);
+      assert.equal(tree.getSize(), 0);
+    });
+  });
+
+  describe('insert', function() {
+    it('Should insert an element in the tree', function() {
+      tree.insert(parent, value);
+      assert.equal(tree.getSize(), 1);
+    });
+  });
+});
